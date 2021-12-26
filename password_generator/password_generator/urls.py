0d5066e41b23df9460a4d1f505369c4a6242e487
Teodor <teodorwis90@gmail.com>
2021-12-26 17:50:30 +0100

    adding name to the home path

--- password_generator/password_generator/urls.py
+++ password_generator/password_generator/urls.py
@@ -19,7 +19,7 @@ from generator import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
+    path('', views.home, name='home'),
     path('eggs', views.eggs),
     path('generatedpassword/', views.password, name='password')
 ]
