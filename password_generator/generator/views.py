0c34ade18e32b291f9bcd65ef923d6f98acb8aad
Teodor <teodorwis90@gmail.com>
2021-12-26 17:50:42 +0100

    adding special characters

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -15,7 +15,17 @@ def eggs(request):
 def password(request):
     generated_password = ''
     characters = list(string.ascii_lowercase)
-    length = 10
+    length = int(request.GET.get('length', 10))
+    if request.GET.get('uppercase'):
+        upper_chars = list(string.ascii_uppercase)
+        characters.extend(upper_chars)
+    if request.GET.get('special'):
+        special_chars = ["<",">","-","/","@","!","~","#","&","?","`",
+                         ",",":",";","(",")","[","]","*","^",".","?","+","%"]
+        characters.extend(special_chars)
+    if request.GET.get('numbers'):
+        numbers = [str(el) for el in range(10)]
+        characters.extend(numbers)
     for x in range(length):
         generated_password += random.choice(characters)
     return render(request, 'generator/password.html', {'password': generated_password})
