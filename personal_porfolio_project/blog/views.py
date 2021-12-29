d64ba2ca573756bb80d0119bda3bbbdc05690a6e
Teodor <teodorwis90@gmail.com>
2021-12-29 16:08:25 +0100

    preparing data model

--- personal_porfolio_project/blog/views.py
+++ personal_porfolio_project/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-# Create your views here.
+
+def all_blogs(request):
+    posts = Blog.objects.order_by("-date")
+
+    return render(request, 'blog/all_blogs.html', {'posts': posts})
+
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'blog': blog})
